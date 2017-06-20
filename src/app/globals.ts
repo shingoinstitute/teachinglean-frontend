6868ef10889b2c8d4f5b6efff3eef92895ea9d73
Craig Blackburn <cr.blackburn89@gmail.com>
2017-06-20 12:31:12 -0600

    global var update

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,10 +1,6 @@
 // global.ts
 import { environment } from '../environments/environment';
 
-export const Globals = {
-  baseApiUrl: 'http://localhost:3000/backend'
-}
-
-// export const Globals = Object.freeze({
-//   baseApiUrl: environment.production ? 'http://teachinglean.org/backend/' : 'http://localhost:3000/backend'
-// });
+export const Globals = Object.freeze({
+  baseApiUrl: environment.production ? 'http://teachinglean.org/backend/' : 'http://localhost:3000/backend'
+});
