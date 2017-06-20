6e320b158b70cabcc5a4093a7db38e4d4c5b98e4
Craig Blackburn <cr.blackburn89@gmail.com>
2017-06-20 17:22:33 -0600

    fixed issue with typings for jquery and tinyMCE

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,8 +1,8 @@
-/// <reference path="../node_modules/@types/jquery/index.d.ts" />
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+/// <reference path="../typings/modules/tinymce/index.d.ts" />
 
 /* SystemJS module definition */
 declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
-declare var tinymce: any;
