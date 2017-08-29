9a042b44bd00e166debcfcbf54854b28eab6fc1c
Craig Blackburn <cr.blackburn89@gmail.com>
2017-08-29 08:41:56 -0600

    added copy changes on homepage layout

--- src/app/config/app-routing.module.ts
+++ src/app/config/app-routing.module.ts
@@ -6,7 +6,7 @@ import { AboutComponent } from '../about/about.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { LoginComponent } from '../login/login.component';
 import { TeachingComponent } from '../teaching/teaching.component';
-import { ForumComponent } from "../forum-home/forum.component";
+import { ForumComponent } from '../forum-home/forum.component';
 import { ForumListItemComponent } from '../forum-list-item/forum-list-item.component';
 import { SignupComponent } from '../signup/signup.component';
 import { ResetPasswordLinkComponent } from '../reset-password-link/reset-password-link.component';
